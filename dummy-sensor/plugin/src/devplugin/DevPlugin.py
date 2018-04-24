931631e7234ed8c3c159df8443c119fa0ff1d9af
Micke Prag <micke.prag@telldus.se>
2018-04-24 14:43:35 +0200

    Pylint fixes

--- dummy-sensor/plugin/src/devplugin/DevPlugin.py
+++ dummy-sensor/plugin/src/devplugin/DevPlugin.py
@@ -1,39 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from base import Plugin, Application
-from telldus import DeviceManager, Device, Sensor
-from threading import Thread
 import logging
+from threading import Thread
+
+from base import Plugin, Application
+from telldus import DeviceManager, Sensor
 
 class DummySensor(Sensor):
-	'''All devices exported must subclass Device
+	'''All sensors exported must subclass Device or Sensor'''
 
-	Minimal function to reimplement is:
-	_command
-	localId
-	typeString
-	methods
-	'''
-	def _command(self, action, value, success, failure, **kwargs):
-		'''This method is called when someone want to control this device
-
-		action is the method id to execute. This could be for instance:
-		Device.TURNON or Device.TURNOFF
-
-		value us only used for some actions, for example dim
-
-		This method _must_ call either success or failure
-		'''
-		logging.debug('Sending command %s to dummy sensor device', action)
-		success()
-
-	def localId(self):
+	@staticmethod
+	def localId():
 		'''Return a unique id number for this device. The id should not be
 		globally unique but only unique for this device type.
 		'''
 		return 1
 
-	def typeString(self):
+	@staticmethod
+	def typeString():
 		'''Return the device type. Only one plugin at a time may export devices using
 		the same typestring'''
 		return 'testsensor'
@@ -61,13 +45,10 @@ class DevPlugin(Plugin):
 			logging.warning("Schedule sensor test")
 			#If the sensor data returned this method will call automatically
 			self.sensor.setSensorValue(Sensor.TEMPERATURE, 15, Sensor.SCALE_TEMPERATURE_CELCIUS)
-		except Exception as e:
+		except Exception as _error:
 			logging.warning("Could not fetch Sensor data")
 
 	def threadTest(self):
-		t = Thread(name='Sensor data', target=self.sensorTest)
-		t.daemon = True
-		t.start()
-
-
-
+		thread = Thread(name='Sensor data', target=self.sensorTest)
+		thread.daemon = True
+		thread.start()
